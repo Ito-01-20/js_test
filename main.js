3bfd463101d22eb0b16c48f6b7c5ea164c925829
Ito-01-20 <kaiki_2001_hebi@icloud.com>
2022-06-17 15:41:09 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const array = [
+    ["赤", "青"],
+    []
+];
+
+console.log(array[0])
+
+const member = {
+    'name': '本田',
+    'height': '170'
+};
+
+console.log(member)
+
+if (height == 90){
+    console.log('身長')
+}
+
