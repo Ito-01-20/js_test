f4b99803347ed2068cc2ec7ad0342cc9d9b6300c
Ito-01-20 <kaiki_2001_hebi@icloud.com>
2022-06-18 22:40:54 +0900

    add new file

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
-const array = [
-    ["赤", "青"],
-    []
-];
+// const array = [
+//     ["赤", "青"],
+//     []
+// ];
 
-console.log(array[0])
+// console.log(array[0])
 
-const member = {
-    'name': '本田',
-    'height': '170'
-};
+// const member = {
+//     'name': '本田',
+//     'height': '170'
+// };
 
-console.log(member)
+// console.log(member)
 
-if (height == 90){
-    console.log('身長')
-}
+// if (height == 90){
+//     console.log('身長')
+// }
+
 
