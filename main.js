f81deff2000d0890c0cf9887d61791589075347a
Ito-01-20 <kaiki_2001_hebi@icloud.com>
2022-06-21 11:54:21 +0900

    add new code

--- main.js
+++ main.js
@@ -17,3 +17,21 @@ while (index <= 10) {
     console.log(index);
     index += 1;
 }
+
+function price(unitPrice, n) {
+    return unitPrice * n
+}
+
+console.log(price(100, 15))
+
+
+let scores = [100, 90, 80, 70, 60];
+let sum = 0;
+
+for (let i = 0; i < scores.length; i++) {
+    sum += scores[i];
+  }
+
+let avg = sum / scores.length;
+console.log('合計値:' + sum);
+console.log('平均値:' + avg);
